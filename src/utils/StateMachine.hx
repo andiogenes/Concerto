808715c239b95c16147f42457f62caf32304890f
LAPTOP-VACUB08O\Антон <megadeathlightsaber@gmail.com>
2019-06-30 20:43:42 +0700

    Implemented state machine abstract classes

---
+++ src/utils/StateMachine.hx
@@ -0,0 +1,61 @@
+package utils;
+
+class StateOf<T> {
+    private var fsm: StateMachineOf<T>;
+    private var owner: T;
+
+    private function new(fsm: StateMachineOf<T>) {
+        setFSM(fsm);
+        setOwner(fsm.getOwner());
+    }
+
+    public function update() {}
+
+    public function getOwner() {
+        return owner;
+    }
+
+    public function setOwner(owner: T) {
+        this.owner = owner;
+    }
+
+    public function getFSM(): StateMachineOf<T> {
+        return fsm;
+    }
+
+    public function setFSM(fsm: StateMachineOf<T>) {
+        this.fsm = fsm;
+    }
+
+}
+
+class StateMachineOf<T> {
+    private var state: StateOf<T> = null;
+    private var owner: T;
+
+    private function new(owner: T) {
+        this.owner = owner;
+    }
+
+    public function update() {
+        if (state != null) {
+            state.update();
+        }
+    }
+
+    public function getState(): StateOf<T> {
+        return state;
+    }
+
+    public function setState(state: StateOf<T>) {
+        this.state = state;
+    }
+
+    public function setOwner(owner: T) {
+        this.owner = owner;
+    }
+
+    public function getOwner(): T {
+        return owner;
+    }
+}
