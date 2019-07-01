e98c383838736c99b711f901c8a8cbc38ed956a3
LAPTOP-VACUB08O\Антон <megadeathlightsaber@gmail.com>
2019-07-01 08:17:22 +0700

    Musician state machine

--- src/model/entities/members/Musician.hx
+++ src/model/entities/members/Musician.hx
@@ -1,8 +1,11 @@
 package model.entities.members;
 
+import utils.StateMachine.StateMachineOf;
 import utils.Queue;
 
 class Musician extends ConcertMember {
+    private var fsm: StateMachineOf<Musician>;
+
     private var songs: Queue<Song>;
 
     public var name(default, null): String;
@@ -13,9 +16,11 @@ class Musician extends ConcertMember {
         this.name = name;
         
         songs = new Queue<Song>();
+        fsm = new StateMachineOf<Musician>(this);
     }
 
     override public function update() {
+        fsm.update();
     }
 
     public function addSong(song: Song) {
