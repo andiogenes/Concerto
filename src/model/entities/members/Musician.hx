baa84c1953ba83fc9733fc1c9347c51b3cb84b4c
LAPTOP-VACUB08O\Антон <megadeathlightsaber@gmail.com>
2019-07-01 08:47:24 +0700

    Added public tuneUpDuration properity in class Musician

--- src/model/entities/members/Musician.hx
+++ src/model/entities/members/Musician.hx
@@ -10,11 +10,13 @@ class Musician extends ConcertMember {
     private var songs: Queue<Song>;
 
     public var name(default, null): String;
+    public var tuneUpDuration(default, set): Int;
 
-    public function new(x: Float, y: Float, name: String, ?stage: Stage) {
+    public function new(x: Float, y: Float, name: String, duration: Int = 0, ?stage: Stage) {
         super(x, y, stage);
 
         this.name = name;
+        this.tuneUpDuration = duration;
         
         songs = new Queue<Song>();
         fsm = new StateMachineOf<Musician>(this);
@@ -30,5 +32,13 @@ class Musician extends ConcertMember {
 
     public function informAboutPerformance() {
         fsm.setState(new MusicianWalkState(fsm));
+    }
+
+    function set_tuneUpDuration(_duration: Int): Int {
+        if (_duration >= 0) {
+            tuneUpDuration = _duration;
+        }
+
+        return tuneUpDuration;
     }
 }
