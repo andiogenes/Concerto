85959a37e0a8720c0a5e65a3a4eb52be7f7a56ac
LAPTOP-VACUB08O\Антон <megadeathlightsaber@gmail.com>
2019-07-01 08:48:48 +0700

    Implemented musician walk state

---
+++ src/model/states/MusicianWalkState.hx
@@ -0,0 +1,32 @@
+package model.states;
+
+import model.entities.Stage;
+import utils.StateMachine.StateMachineOf;
+import utils.StateMachine.StateOf;
+import model.entities.members.Musician;
+
+class MusicianWalkState extends StateOf<Musician> {
+    private static final MIN_DISTANCE: Float = 5;
+    
+    var stage: Stage;
+
+    public function new(fsm: StateMachineOf<Musician>) {
+        super(fsm);
+
+        stage = owner.getStage();
+    }
+
+    override public function update() {
+        var vecX = stage.x - owner.x;
+        var vecY = stage.y - owner.y;
+        var length = Math.sqrt(vecX*vecX + vecY*vecY);
+
+        if (length <= MIN_DISTANCE) {
+            fsm.setState(new MusicianTuneUpState(fsm));
+            return;
+        }
+
+        owner.x += vecX / length;
+        owner.y += vecY / length;
+    }
+}
