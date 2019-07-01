bc8bfb7cfc02a449df882aa46686998b22b9f3fc
LAPTOP-VACUB08O\Антон <megadeathlightsaber@gmail.com>
2019-07-01 10:40:21 +0700

    Implemented listener's walk to store state

---
+++ src/model/states/ListenerWalkToStoreState.hx
@@ -0,0 +1,59 @@
+package model.states;
+
+import model.entities.Stage;
+import model.entities.members.Listener;
+import utils.StateMachine;
+import model.entities.MerchStore;
+
+class ListenerWalkToStoreState extends StateOf<Listener> {
+    private static final MIN_DISTANCE: Float = 5;
+
+    var stage: Stage;
+    var store: MerchStore;
+
+    public function new(fsm: StateMachineOf<Listener>) {
+        super(fsm);
+
+        stage = owner.getStage();
+        store = owner.getStore();
+    }
+
+    override public function update() {
+        // No musucian on stage => 
+        // concert ended => 
+        // switch to 'leave concert' state
+        if (stage.currentMusician() == null) {
+            fsm.setState(new ListenerLeaveState(fsm));
+            return;
+        }
+
+        var vecX = store.x - owner.x;
+        var vecY = store.y - owner.y;
+        var length = Math.sqrt(vecX*vecX + vecY*vecY);
+
+        // Listener reachs store =>
+        // try to buy some merch and switch to 'walk to scene' state
+        if (length <= MIN_DISTANCE) {
+            // Listener buys item what his or her wallet allows
+            var merch = store.getMerchandise();
+
+            for (v in merch) {
+                if (v.price <= owner.money) {
+                    var item = store.dispense(v);
+                    
+                    owner.money -= item.price;
+                    owner.mood += item.quality;
+                    
+                    break;
+                }
+            }
+
+            fsm.setState(new ListenerWalkToSceneState(fsm));
+            return;
+        }
+
+        // Move to store
+        owner.x += vecX / length;
+        owner.y += vecY / length;
+    }
+}
