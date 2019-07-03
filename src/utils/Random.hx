fa08076d75a396a1de394029cb52ed1a91dc6f6b
LAPTOP-VACUB08O\Антон <megadeathlightsaber@gmail.com>
2019-07-03 09:54:20 +0700

    randomizeF for floats

--- src/utils/Random.hx
+++ src/utils/Random.hx
@@ -4,4 +4,8 @@ class Random {
     public static function randomize(min: Int, max: Int): Int {
 		return min + Math.round(Math.random() * (max - min));
 	}
+
+	public static function randomizeF(min: Float, max: Float): Float {
+		return min + Math.random() * (max - min);
+	}
 }
