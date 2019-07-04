42beef44dfb2df755141c00d58945c2eb222c17e
LAPTOP-VACUB08O\Антон <megadeathlightsaber@gmail.com>
2019-07-05 01:29:03 +0700

    Setted up listener's mood equations

--- src/model/states/ListenerListenState.hx
+++ src/model/states/ListenerListenState.hx
@@ -9,7 +9,14 @@ class ListenerListenState extends StateOf<Listener> {
     private static final MOOD_REFRESH_INTERVAL: Float = 5;
     private static final TIME_STAMP: Float = 0.05;
 
+    private static final SOUND_QUALITY_PENALTY: Int = 20;
+    private static final MUSIC_QUALITY_PENALTY: Int = 30;
+    private static final MAXIMAL_WAITING_DURATION: Int = 60;
+    private static final WAITING_STEP: Float = 0.05;
+
     var elapsed: Float = 0;
+
+    var waitingMoodDecay = 0.;
 
     var stage: Stage;
     var cone: SoundPropagationCone;
@@ -38,8 +45,32 @@ class ListenerListenState extends StateOf<Listener> {
         }
 
         if (elapsed >= MOOD_REFRESH_INTERVAL) {
-            // MOOD = SONG_QUALITY + SOUND_QUALITY - DISTANCE_PENALTY - WAITING
-            owner.mood += 5;
+            // Equation of listener's mood from stage's means quality
+            owner.mood += Std.int((stage.soundQuality - SOUND_QUALITY_PENALTY) / 10);
+
+            if (stage.isInPreparation) {
+                // Equation of mood from tune up duration
+                waitingMoodDecay += WAITING_STEP;
+                owner.mood -= Std.int(1/(MAXIMAL_WAITING_DURATION - waitingMoodDecay)-(1/MAXIMAL_WAITING_DURATION));
+            } else {
+                // Equation of mood from musician, songs
+
+                if (waitingMoodDecay != 0) {
+                    waitingMoodDecay = 0;
+                }
+
+                owner.mood += Std.int((stage.musicQuality - MUSIC_QUALITY_PENALTY) / 10);
+
+                var distanceX = stage.x - owner.x;
+                var distanceY = stage.y - owner.y;
+                var distanceToScene = Math.sqrt(distanceX*distanceX+distanceY*distanceY);
+
+                // Equation of mood from remoteness of scene
+                if (distanceToScene > cone.radius) {
+                    owner.mood -= Std.int((distanceToScene / cone.radius) * 5);
+                }
+            }
+
             elapsed = 0;
         }
 
